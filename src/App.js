4b60eaad43f56ff53c1d77bba53426b2f4d53884
Avinash Tiwari <aviprf10@gmail.com>
2023-12-24 23:24:43 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const LocationSelector = () => {
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [states, setStates] = useState([]);
+  const [selectedState, setSelectedState] = useState('');
+  const [cities, setCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState('');
+
+  useEffect(() => {
+    // Fetch countries on initial render
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch('https://crio-location-selector.onrender.com/countries');
+      const data = await response.json();
+      setCountries(data);
+    } catch (error) {
+      console.error('Error fetching countries:', error);
+    }
+  };
+
+  const fetchStates = async (countryName) => {
+    try {
+      const response = await fetch(`https://crio-location-selector.onrender.com/country=${countryName}/states`);
+      const data = await response.json();
+      setStates(data);
+    } catch (error) {
+      console.error(`Error fetching states for ${countryName}:`, error);
+    }
+  };
+
+  const fetchCities = async (countryName, stateName) => {
+    try {
+      const response = await fetch(`https://crio-location-selector.onrender.com/country=${countryName}/state=${stateName}/cities`);
+      const data = await response.json();
+      setCities(data);
+    } catch (error) {
+      console.error(`Error fetching cities for ${countryName}, ${stateName}:`, error);
+    }
+  };
+
+  const handleCountryChange = (e) => {
+    const countryName = e.target.value;
+    setSelectedCountry(countryName);
+    setSelectedState('');
+    setSelectedCity('');
+
+    // Fetch states for the selected country
+    fetchStates(countryName);
+  };
+
+  const handleStateChange = (e) => {
+    const stateName = e.target.value;
+    setSelectedState(stateName);
+    setSelectedCity('');
+
+    // Fetch cities for the selected country and state
+    fetchCities(selectedCountry, stateName);
+  };
+
+  const handleCityChange = (e) => {
+    const cityName = e.target.value;
+    setSelectedCity(cityName);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Location Selector</h1>
+      <div>
+        <label>Select Country:</label>
+        <select value={selectedCountry} onChange={handleCountryChange}>
+          <option value="">-- Select Country --</option>
+          {countries.map((country) => (
+            <option key={country} value={country}>
+              {country}
+            </option>
+          ))}
+        </select>
+      </div>
+      {selectedCountry && (
+        <>
+          <div>
+            <label>Select State:</label>
+            <select value={selectedState} onChange={handleStateChange}>
+              <option value="">-- Select State --</option>
+              {states.map((state) => (
+                <option key={state} value={state}>
+                  {state}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label>Select City:</label>
+            <select value={selectedCity} onChange={handleCityChange} disabled={!selectedState}>
+              <option value="">-- Select City --</option>
+              {cities.map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+          </div>
+          {selectedCity && (
+            <div>
+              <p>You Selected {selectedCity}, {selectedState}, {selectedCountry}</p>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default LocationSelector;
